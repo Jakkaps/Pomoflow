7db239e312de4a8e8cfb49f919485ef47eab573e
Jens Amund <jens@jenss-macbook-pro.local>
2018-10-11 09:37:53 +0200

    You can now start a timer

--- Pomoflow/AppDelegate.swift
+++ Pomoflow/AppDelegate.swift
@@ -12,17 +12,26 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    let menuItem = NSMenuItem(title: "Start Timer", action: #selector(AppDelegate.startTimer(_:)), keyEquivalent: "P")
+    
+    var time = 25
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        if let button = statusItem.button {
-            button.action = #selector(buttonClicked(_:))
-        }
-        
         constructMenu()
     }
     
-    @objc func buttonClicked(_ sender: Any){
-        print("Clicked")
+    @objc func startTimer(_ sender: Any){
+        menuItem.title = "\(time) minutes remaining"
+        menuItem.action = nil
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.time -= 1
+            self.menuItem.title = "\(self.time) minutes remaining"
+            
+            if self.time == 0 {
+                timer.invalidate()
+            }
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -32,7 +41,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func constructMenu() {
         let menu = NSMenu()
         
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.buttonClicked(_:)), keyEquivalent: "P"))
+        menu.addItem(menuItem)
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         
