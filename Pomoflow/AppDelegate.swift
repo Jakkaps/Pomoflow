65ce1b9d412ee1efda1c427defebb61effb03630
Jens Amund <me@jakkaps.com>
2019-01-09 20:29:59 +0100

    Did the foundation for the rewrite: transferring functionality to the class PomoflowTimer

--- Pomoflow/AppDelegate.swift
+++ Pomoflow/AppDelegate.swift
@@ -11,35 +11,21 @@ import UserNotifications
 
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate{
-
+class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate, PomoflowTimerDelegate{
     
     @IBOutlet weak var timeItem: NSMenuItem!
     @IBOutlet weak var startStopItem: NSMenuItem!
+    @IBOutlet weak var skipItem: NSMenuItem!
     @IBOutlet weak var pauseContinueItem: NSMenuItem!
     @IBOutlet weak var prefsItem: NSMenuItem!
     
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     @IBOutlet weak var menu: NSMenu!
     
-    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
-    
-    var pomodoroLength = 25
-    var workLength = 120
-    var breakLength = 5
-    
-    var remainingTimeSession = 0
-    var remainingTimeWork = 0
-    
-    var onBreak = false
-    var started = false
-    var isPaused = false
-
-    var workTimer: Timer?
-    var pomodoroBreakTimer: Timer?
-    
+    var currentTimer : PomoflowTimer!
     var stringToEncourageWork = "Ya not working"
-    
     var prefs = Preferences()
+    var active = false
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         if #available(OSX 10.14, *) {
@@ -65,18 +51,11 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
             
         }
         
-        setTimesFromPreferences()
+        
+        currentTimer = prefs.returnSelectedTimer()
         resetMenu()
         listenForPrefsChanged()
-        timeItem.title = stringToEncourageWork
         statusItem.menu = menu
-    }
-    
-    func setTimesFromPreferences(){
-        let currentTimer = prefs.returnSelectedTimer()
-        workLength = currentTimer.workLength
-        pomodoroLength = currentTimer.pomodoroLength
-        breakLength = currentTimer.breakLength
     }
     
     @IBAction func quitClicked(_ sender: Any) {
@@ -86,16 +65,39 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
     fileprivate func resetMenu() {
         let icon = NSImage(named: "statusIcon")
         statusItem.image = icon
+
         timeItem.title = stringToEncourageWork
-        startStopItem.title = "Start"
+        
+        skipItem.isEnabled = false
         pauseContinueItem.isEnabled = false
-        
-        onBreak = false
-        isPaused = false
-        started = false
-        
-        workTimer?.invalidate()
-        pomodoroBreakTimer?.invalidate()
+        changeDisplayedTimers()
+    }
+    
+    @objc func differentPresetSelcted(sender: NSMenuItem){
+        let newlySelectedPresetIndex = menu.index(of: sender) - 5
+        prefs.selected = newlySelectedPresetIndex
+        prefs.save()
+        resetMenu()
+        currentTimer.stop()
+        currentTimer = prefs.returnSelectedTimer()
+        changeDisplayedTimers()
+    }
+    
+    func listenForPrefsChanged(){
+        let notificationName = Notification.Name(rawValue: "PrefsChanged")
+        NotificationCenter.default.addObserver(forName: notificationName,
+                                               object: nil, queue: nil) {
+                                                (notification) in
+                                                self.prefs = Preferences()
+                                                self.currentTimer.stop()
+                                                self.resetMenu()
+                                                self.changeDisplayedTimers()
+                                                
+        }
+    }
+    
+    func changeDisplayedTimers(){
+        menu.insertItem(NSMenuItem.separator(), at: 5)
         
         //First you have to remove the old presets
         for item in menu.items{
@@ -106,7 +108,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
         
         let timers = prefs.returnAllTimers()
         //Start insering the items 4 steps into the menu
-        var menuPositionToInsertAt = 5
+        var menuPositionToInsertAt = 6
         for (index, timer) in timers.enumerated() {
             let name = "\(index + 1): \(PomoflowTimer.returnAsHours(min: timer.workLength))/\(timer.pomodoroLength)m/\(timer.breakLength)m"
             let menuItem = NSMenuItem(title: name, action: #selector(differentPresetSelcted(sender:)), keyEquivalent: "")
@@ -119,133 +121,52 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
             }
             
             menu.insertItem(menuItem, at: menuPositionToInsertAt)
-
+            
             menuPositionToInsertAt += 1
         }
         menu.insertItem(NSMenuItem.separator(), at: menuPositionToInsertAt)
     }
     
-    @objc func differentPresetSelcted(sender: NSMenuItem){
-        let newlySelectedPresetIndex = menu.index(of: sender) - 5
-        prefs.selected = newlySelectedPresetIndex
-        prefs.save()
-        resetMenu()
-        setTimesFromPreferences()
-    }
-    
-    func listenForPrefsChanged(){
-        let notificationName = Notification.Name(rawValue: "PrefsChanged")
-        NotificationCenter.default.addObserver(forName: notificationName,
-                                               object: nil, queue: nil) {
-                                                (notification) in
-                                                self.workTimer?.invalidate()
-                                                self.pomodoroBreakTimer?.invalidate()
-                                                self.prefs = Preferences()
-                                                print(self.prefs.selected)
-                                                self.setTimesFromPreferences()
-                                                self.resetMenu()
-                                                
-        }
-    }
-    
     @IBAction func startStopClicked(_ sender: Any) {
-        if !started {
-            startTimers(sessionTime: pomodoroLength, workTime: workLength)
+        if !active {
+            currentTimer.delegate = self
+            currentTimer.start()
+            timeItem.title = "\(currentTimer.pomodoroLength) : \(currentTimer.workLength)"
+            statusItem.image = NSImage(named: "statusIconRunning")
+            skipItem.isEnabled = true
             pauseContinueItem.isEnabled = true
-            startStopItem.title = "Stop"
-            started = true
-            
-            for item in menu.items{
-                if item.tag == 1{
-                    menu.removeItem(item)
-                }
-            }
+    
         }else{
-            if remainingTimeSession <= 0 {
-                startXEndOfSessionClicked()
-            }else {
-                started = false
-                pomodoroBreakTimer?.invalidate()
-                workTimer?.invalidate()
-                resetMenu()
-            }
+            currentTimer.stop()
+            resetMenu()
         }
+        
+        active = !active
     }
     
     @IBAction func pauseContinueClicked(_ sender: Any) {
-        if !isPaused {
-            pomodoroBreakTimer?.invalidate()
-            workTimer?.invalidate()
-            startStopItem.isEnabled = false
-            pauseContinueItem.title = "Continue"
-        }else{
-            startStopItem.isEnabled = true
-            startTimers(sessionTime: remainingTimeSession, workTime: remainingTimeWork)
-            pauseContinueItem.title = "Pause"
-        }
         
-        isPaused = !isPaused
     }
     
-    
-    func endOfSessionReached(){
-        let whatToStart = (onBreak ? "pomodoro" : "break")
-        startStopItem.title = "Start \(whatToStart)"
-        sendNotification(title: "Time for a \(whatToStart)", withSound: false)
-    }
-    
-    func endOfWorkSessionReached(){
-        workTimer?.invalidate()
-        pomodoroBreakTimer?.invalidate()
-        startStopClicked(self)
-        sendNotification(title: "YOU ARE DONE", withSound: true)
-    }
-    
-    func startXEndOfSessionClicked(){
-        remainingTimeSession = (onBreak ? pomodoroLength : breakLength)
-        startStopItem.title = "Stop"
-        self.onBreak = !self.onBreak
-        updateTimeItem()
-    }
-    
-    private func updateTimeItem(){
-        self.timeItem.title = "\(remainingTimeSession >= 0 ? remainingTimeSession : 0)m : \(PomoflowTimer.returnAsHours(min: self.remainingTimeWork))"
-    }
-    
-    @objc func startTimers(sessionTime: Int, workTime: Int){
-        statusItem.image = NSImage(named: "statusIconRunning")
+    @IBAction func skipClicked(_ sender: Any) {
         
-        remainingTimeWork = workTime
-        remainingTimeSession = sessionTime
-        
-        updateTimeItem()
-        
-        startBreakPomdoroTimer(sessionTime: remainingTimeSession)
-        startWorkTimer(workTime: remainingTimeWork)
     }
     
-    func startBreakPomdoroTimer(sessionTime: Int){
-        remainingTimeSession = sessionTime
-        pomodoroBreakTimer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { timer in
-            self.remainingTimeSession -= 1
-            self.updateTimeItem()
-            
-            if self.remainingTimeSession == 0 {
-                self.endOfSessionReached()
-            }
-        }
+    func workFinished() {
+        sendNotification(title: "Donezoo", withSound: true)
     }
     
-    func startWorkTimer(workTime: Int){
-        remainingTimeWork = workTime
-        workTimer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { timer in
-            self.remainingTimeWork -= 1
-            self.updateTimeItem()
-            
-            if self.remainingTimeWork == 0 {
-                self.endOfWorkSessionReached()
-            }
-        }
+    func pomodoroFinished() {
+        sendNotification(title: "Time for a break!", withSound: false)
+    }
+    
+    func breakFinished() {
+        sendNotification(title: "Time for work yo!", withSound: false)
+    }
+    
+    
+    func updateRemaingTime(workTime: Int, pomodoroOrBreakTime: Int) {
+        timeItem.title = "\(pomodoroOrBreakTime) : \(workTime)"
     }
     
     func sendNotification(title: String, withSound: Bool){
@@ -276,7 +197,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDele
         {
         case "TIMER":
             if response.actionIdentifier == "Start"{
-                startXEndOfSessionClicked()
+                
             }
             
         default:
--- Pomoflow/PomoflowTimer.swift
+++ Pomoflow/PomoflowTimer.swift
@@ -8,10 +8,25 @@
 
 import Foundation
 
+protocol PomoflowTimerDelegate{
+    func workFinished()
+    func pomodoroFinished()
+    func breakFinished()
+    
+    func updateRemaingTime(workTime: Int, pomodoroOrBreakTime: Int)
+}
+
 class PomoflowTimer: NSObject, NSCoding{
+    var delegate: PomoflowTimerDelegate?
+    var workTimer: Timer?
+    var pomodoroOrBreakTimer: Timer?
+    
     var workLength: Int
     var pomodoroLength: Int
     var breakLength: Int
+    
+    var workLengthRemaining = 0
+    var pomodoroOrBreakLengthRemaining = 0
     
     required convenience init(coder decoder: NSCoder){
         let workLength = decoder.decodeInteger(forKey: "workLength")
@@ -31,6 +46,59 @@ class PomoflowTimer: NSObject, NSCoding{
         aCoder.encode(workLength, forKey: "workLength")
         aCoder.encode(pomodoroLength, forKey: "pomodoroLength")
         aCoder.encode(breakLength, forKey: "breakLength")
+    }
+    
+    func start(){
+        workLengthRemaining = workLength
+        pomodoroOrBreakLengthRemaining = pomodoroLength
+        
+        guard let delegate = self.delegate else {
+            print("Timer has no delegate!")
+            return
+        }
+        
+        workTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.workLengthRemaining -= 1
+            
+            if self.workLengthRemaining >= 0 {
+                delegate.updateRemaingTime(workTime: self.workLengthRemaining, pomodoroOrBreakTime: self.pomodoroOrBreakLengthRemaining)
+            }
+            
+            if self.workLengthRemaining == 0 {
+                delegate.workFinished()
+                timer.invalidate()
+                self.pomodoroOrBreakTimer?.invalidate()
+                self.delegate = nil
+            }
+        }
+        
+        var onBreak = false
+        
+        pomodoroOrBreakTimer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true){ timer in
+            self.pomodoroOrBreakLengthRemaining -= 1
+            
+            if self.pomodoroOrBreakLengthRemaining >= 0{
+                delegate.updateRemaingTime(workTime: self.workLengthRemaining, pomodoroOrBreakTime: self.pomodoroOrBreakLengthRemaining)
+            }
+            
+            if self.pomodoroOrBreakLengthRemaining == 0 {
+                if onBreak{
+                    self.pomodoroOrBreakLengthRemaining = self.pomodoroLength
+                    delegate.breakFinished()
+                }else{
+                    self.pomodoroOrBreakLengthRemaining = self.breakLength
+                    delegate.pomodoroFinished()
+                }
+                
+                onBreak = !onBreak
+            }
+        }
+    }
+    
+    func stop(){
+        delegate = nil
+        workTimer?.invalidate()
+        pomodoroOrBreakTimer?.invalidate()
     }
     
     static func returnAsHours(min: Int) -> String{
